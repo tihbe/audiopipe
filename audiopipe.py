6b1c9189379f7fd28363ec0769c8a069fd3ba335
Ismael Balafrej <9361364+tihbe@users.noreply.github.com>
2021-04-17 20:55:50 -0400

    Support for named devices

--- audiopipe.py
+++ audiopipe.py
@@ -4,16 +4,52 @@ from tqdm import tqdm
 from fire import Fire
 
 DEFAULT_PORT = 63123
+DEFAULT_BITRATE = 48000
 
-def server(bind_ip="0.0.0.0", port=DEFAULT_PORT, rate=48000, channels=2, block_size=4096, verbose=True):
+
+def devices(p=pyaudio.PyAudio()):
+    for i in range(p.get_device_count()):
+        yield i, p.get_device_info_by_index(i)
+
+
+def get_device_index_by_name(name, p=pyaudio.PyAudio()):
+    for i, device in devices(p):
+        if name.lower() in device["name"].lower():
+            return i
+
+    return None
+
+
+def print_devices():
+    for _, device in devices():
+        print(device["name"])
+
+
+def start_server(
+    bind_ip="0.0.0.0",
+    port=DEFAULT_PORT,
+    rate=DEFAULT_BITRATE,
+    channels=2,
+    block_size=4096,
+    verbose=True,
+    device_name="",
+):
     p = pyaudio.PyAudio()
+    device_index = get_device_index_by_name(device_name, p) if device_name else None
+    if device_name and device_index is None:
+        print(f"Device '{device_name}' not found.")
+        exit(1)
 
-    stream = p.open(format=pyaudio.paInt16,
-                    channels=channels,
-                    rate=rate,
-                    output=True)
-
-    pbar = tqdm(desc="Data received", unit="bytes", unit_scale=True, disable=not verbose)
+    stream = p.open(
+        format=pyaudio.paInt16,
+        channels=channels,
+        rate=rate,
+        output=True,
+        output_device_index=device_index,
+    )
+    pbar = tqdm(
+        desc="Data received", unit="bytes", unit_scale=True, disable=not verbose
+    )
 
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
@@ -29,27 +65,29 @@ def server(bind_ip="0.0.0.0", port=DEFAULT_PORT, rate=48000, channels=2, block_s
         pbar.close()
 
 
-def client(ip="127.0.0.1", port=DEFAULT_PORT, rate=48000, channels=2, block_size=1024, verbose=True):
+def start_client(
+    ip="127.0.0.1",
+    port=DEFAULT_PORT,
+    rate=DEFAULT_BITRATE,
+    channels=2,
+    block_size=1024,
+    verbose=True,
+    device_name="stereo mix",
+):
     p = pyaudio.PyAudio()
-    stereo_mix_index = None
-
-    for i in range(p.get_device_count()):
-        device = p.get_device_info_by_index(i)
-        if "stereo mix" in device["name"].lower():
-            stereo_mix_index = i
-            break
-
-    if stereo_mix_index is None:
-        print("Stereo mix not found")
+    device_index = get_device_index_by_name(device_name, p) if device_name else None
+    if device_name and device_index is None:
+        print(f"Device '{device_name}' not found.")
         exit(1)
 
-    stream = p.open(format=pyaudio.paInt16,
-                    channels=channels,
-                    rate=rate,
-                    input_device_index=stereo_mix_index,
-                    input=True,
-                    frames_per_buffer=block_size)
-
+    stream = p.open(
+        format=pyaudio.paInt16,
+        channels=channels,
+        rate=rate,
+        input=True,
+        input_device_index=device_index,
+        frames_per_buffer=block_size,
+    )
     pbar = tqdm(desc="Data sent", unit="bytes", unit_scale=True, disable=not verbose)
 
     try:
@@ -66,4 +104,6 @@ def client(ip="127.0.0.1", port=DEFAULT_PORT, rate=48000, channels=2, block_size
 
 
 if __name__ == "__main__":
-    Fire({"client": client, "server": server})
+    Fire(
+        {"client": start_client, "server": start_server, "list_devices": print_devices}
+    )
